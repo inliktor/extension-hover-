d84a643eaa560a14e58d97fc5a7acb2dd6745931
chekizybra <ask,engelc@mail.ru>
2024-08-23 19:13:34 +0400

    стили вынесены в отдельный файл

--- content.js
+++ content.js
@@ -1,19 +1,26 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "applyRotation") {
-      var elements = document.getElementsByTagName(request.tagName);
-      var css = `
+      const elements = document.getElementsByTagName(request.tagName);
+      
+      if (elements.length === 0) {
+        console.log(`Элементы с тегом ${request.tagName} не найдены.`);
+        return;
+      }
+  
+      const css = `
         @keyframes rotate {
           from { transform: rotate(0deg); }
           to { transform: rotate(${request.degrees}deg); }
         }
       `;
-      var style = document.createElement('style');
+      const style = document.createElement('style');
       style.textContent = css;
       document.head.appendChild(style);
   
-      for (var i = 0; i < elements.length; i++) {
+      for (let i = 0; i < elements.length; i++) {
         elements[i].style.animation = `rotate ${request.duration}s linear infinite`;
         elements[i].style.display = 'inline-block';
+        elements[i].style.animationPlayState = 'paused';
         
         elements[i].addEventListener('mouseover', function() {
           this.style.animationPlayState = 'running';
--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var tagName = document.getElementById('tagName').value;
       var duration = document.getElementById('duration').value;
       var degrees = document.getElementById('degrees').value;
-      
+  
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
           action: "applyRotation",
